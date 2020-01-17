455382e256bd69bd38342b544a2e6d5c4102b456
Mirek Simek <miroslav.simek@vscht.cz>
2020-01-17 09:03:26 +0100

    working on invenio 3.2 compatibility

--- setup.py
+++ setup.py
@@ -14,17 +14,9 @@ from setuptools import find_packages, setup
 readme = open('README.rst').read()
 
 DATABASE = "postgresql"
-INVENIO_VERSION = "3.1.0"
+OAREPO_VERSION = os.environ.get('OAREPO_VERSION', '3.1.1')
 
 tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=4.0',
-    'isort>=4.3.3',
-    'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
-    'pytest-cov>=1.8.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
 ]
 
 setup_requires = [
@@ -33,12 +25,19 @@ setup_requires = [
 
 install_requires = [
     'Flask-BabelEx>=0.9.3',
-    'invenio-records-rest>=1.1.0',
     'arrow>=0.12.1',
+    'invenio-db>=1.0.0',
     'invenio_oauthclient>=1.0.1',
-    'invenio[{db},base,auth]~={version}'.format(
-        db=DATABASE, version=INVENIO_VERSION),
 ]
+
+extras_require = {
+    'tests': [
+        'oarepo[tests]~={version}'.format(
+            version=OAREPO_VERSION)],
+    'tests-es7': [
+        'oarepo[tests-es7]~={version}'.format(
+            version=OAREPO_VERSION)],
+}
 
 packages = find_packages()
 
@@ -71,6 +70,7 @@ setup(
     install_requires=install_requires,
     setup_requires=setup_requires,
     tests_require=tests_require,
+    extras_require=extras_require,
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
